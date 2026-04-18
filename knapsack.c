7320362e0dd0117fa5775ad4f28f2382b6a2d050
Tonmoy298 <tonmoyofs@gmail.com>
2026-04-18 10:05:17 +0600

    Add files via upload

---
+++ knapsack.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int max(int a, int b) {
+    return (a > b) ? a : b;
+}
+
+int knapsack(int W, int wt[], int val[], int n) {
+    int i, w;
+    int dp[100][100];
+
+    for(i = 0; i <= n; i++) {
+        for(w = 0; w <= W; w++) {
+            if(i == 0 || w == 0)
+                dp[i][w] = 0;
+            else if(wt[i-1] <= w)
+                dp[i][w] = max(val[i-1] + dp[i-1][w - wt[i-1]], dp[i-1][w]);
+            else
+                dp[i][w] = dp[i-1][w];
+        }
+    }
+
+    return dp[n][W];
+}
+
+int main() {
+    int val[100], wt[100], W, n, i;
+
+    printf("Enter number of items: ");
+    scanf("%d", &n);
+
+    printf("Enter values:\n");
+    for(i = 0; i < n; i++)
+        scanf("%d", &val[i]);
+
+    printf("Enter weights:\n");
+    for(i = 0; i < n; i++)
+        scanf("%d", &wt[i]);
+
+    printf("Enter capacity of knapsack: ");
+    scanf("%d", &W);
+
+    printf("Maximum profit = %d", knapsack(W, wt, val, n));
+
+    return 0;
+}
